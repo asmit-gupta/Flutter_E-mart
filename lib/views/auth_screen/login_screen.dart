671707bce4f6a44ce92c0c490e9c9cd6fba95950
Asmit Gupta <asmitwrites@gmail.com>
2022-11-26 23:45:25 +0530

    added some widgets in login screen

--- lib/views/auth_screen/login_screen.dart
+++ lib/views/auth_screen/login_screen.dart
@@ -1,4 +1,10 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:emart_app/consts/consts.dart';
+import 'package:emart_app/widgets_common/applogo_widget.dart';
+import 'package:emart_app/widgets_common/bg_widget.dart';
+import 'package:emart_app/widgets_common/custom_textfield.dart';
+import 'package:emart_app/widgets_common/our_button.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
@@ -12,6 +18,59 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return bgWidget(
+        child: Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            (context.screenHeight * 0.1).heightBox,
+            applogoWidget(),
+            10.heightBox,
+            "Log in to $appname".text.fontFamily(bold).white.size(18).make(),
+            10.heightBox,
+            Column(
+              children: [
+                customTextField(hint: emailHint, title: email),
+                customTextField(hint: passwordHint, title: password),
+                Align(
+                    alignment: Alignment.centerRight,
+                    child: TextButton(
+                        onPressed: () {}, child: forgetPass.text.make())),
+                5.heightBox,
+                // ourButton(title: "Sign up", color: Colors.red)
+                //     .box
+                //     .width(context.screenWidth - 50)
+                //     .make(),
+                ourButton(
+                        color: redColor,
+                        title: login,
+                        textColor: whiteColor,
+                        onPress: () {})
+                    .box
+                    .width(context.screenWidth - 50)
+                    .make(),
+                5.heightBox,
+                createnewAccount.text.color(fontGrey).make(),
+                ourButton(
+                  color: Color.fromARGB(255, 195, 188, 126),
+                  title: signUp,
+                  textColor: redColor,
+                  onPress: () {},
+                ).box.width(context.screenWidth - 50).make(),
+                10.heightBox,
+                loginwith.text.color(fontGrey).make(),
+                5.heightBox,
+              ],
+            )
+                .box
+                .white
+                .rounded
+                .padding(const EdgeInsets.all(16))
+                .width(context.screenWidth - 70)
+                .make()
+          ],
+        ),
+      ),
+    ));
   }
 }
