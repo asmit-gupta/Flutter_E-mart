7065ff47db26934cfe1f354d5586c9ae0f8fa010
Asmit Gupta <asmitwrites@gmail.com>
2022-11-29 19:33:20 +0530

    added UI of Home Screen

---
+++ lib/views/home_screen/home_screen.dart
@@ -0,0 +1,140 @@
+import 'package:emart_app/consts/colors.dart';
+import 'package:emart_app/consts/lists.dart';
+import 'package:emart_app/widgets_common/home_buttons.dart';
+import 'package:flutter/material.dart';
+import 'package:emart_app/consts/consts.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(12),
+      color: lightGrey,
+      width: context.screenWidth,
+      height: context.screenHeight,
+      child: SafeArea(
+          child: Column(
+        children: [
+          Container(
+            alignment: Alignment.center,
+            height: 60,
+            color: lightGrey,
+            child: TextFormField(
+              decoration: const InputDecoration(
+                border: InputBorder.none,
+                suffixIcon: Icon(Icons.search),
+                filled: true,
+                fillColor: whiteColor,
+                hintText: searchanything,
+                hintStyle: TextStyle(
+                  color: textfieldGrey,
+                ),
+              ),
+            ),
+          ),
+          10.heightBox,
+          Expanded(
+            child: SingleChildScrollView(
+              physics: const BouncingScrollPhysics(),
+              child: Column(
+                children: [
+                  //swiper brands coursel
+                  VxSwiper.builder(
+                      aspectRatio: 16 / 9,
+                      autoPlay: true,
+                      autoPlayAnimationDuration: const Duration(seconds: 2),
+                      height: 150,
+                      enlargeCenterPage: true,
+                      itemCount: slidersList.length,
+                      itemBuilder: (context, index) {
+                        return Image.asset(
+                          slidersList[index],
+                          fit: BoxFit.fill,
+                        )
+                            .box
+                            .rounded
+                            .clip(Clip.antiAlias)
+                            .margin(const EdgeInsets.symmetric(horizontal: 8))
+                            .make();
+                      }),
+
+                  10.heightBox,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: List.generate(
+                        2,
+                        (index) => homeButtons(
+                              height: context.screenHeight * 0.15,
+                              width: context.screenWidth / 2.5,
+                              icon: index == 0 ? icTodaysDeal : icFlashDeal,
+                              title: index == 0 ? todayDeal : flashsale,
+                            )),
+                  ),
+
+                  10.heightBox,
+
+                  //2nd swiper coursel
+                  VxSwiper.builder(
+                      aspectRatio: 16 / 9,
+                      autoPlay: true,
+                      autoPlayAnimationDuration: const Duration(seconds: 5),
+                      height: 150,
+                      enlargeCenterPage: true,
+                      itemCount: secondSliderList.length,
+                      itemBuilder: (context, index) {
+                        return Image.asset(
+                          secondSliderList[index],
+                          fit: BoxFit.fill,
+                        )
+                            .box
+                            .rounded
+                            .clip(Clip.antiAlias)
+                            .margin(const EdgeInsets.symmetric(horizontal: 8))
+                            .make();
+                      }),
+
+                  10.heightBox,
+
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: List.generate(
+                      3,
+                      (index) => homeButtons(
+                          height: context.screenHeight * 0.15,
+                          width: context.screenWidth / 3.5,
+                          icon: index == 0
+                              ? icTopCategories
+                              : index == 1
+                                  ? icBrands
+                                  : icTopSeller,
+                          title: index == 0
+                              ? topcateg
+                              : index == 1
+                                  ? brand
+                                  : topseller),
+                    ),
+                  ),
+
+                  10.heightBox,
+
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: featuredCateg.text
+                        .color(darkFontGrey)
+                        .size(18)
+                        .fontFamily(semibold)
+                        .make(),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      )),
+    );
+  }
+}
